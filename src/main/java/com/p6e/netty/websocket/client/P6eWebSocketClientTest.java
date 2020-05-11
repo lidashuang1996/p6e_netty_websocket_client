e19d3149f150cdb055e7a5e576f70a84307ddf8f
lidashuang <1294935733@qq.com>
2020-05-11 21:34:42 +0800

    update p6e-netty-websocket-client-1.0.2.jar

--- src/main/java/com/p6e/netty/websocket/client/P6eWebSocketClientTest.java
+++ src/main/java/com/p6e/netty/websocket/client/P6eWebSocketClientTest.java
@@ -6,11 +6,6 @@ import com.p6e.netty.websocket.client.config.P6eConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
-import java.util.Hashtable;
-import java.util.List;
-import java.util.Map;
-
 public class P6eWebSocketClientTest {
 
     /** 注入的日志对象 */
@@ -20,10 +15,6 @@ public class P6eWebSocketClientTest {
         P6eWebSocketClientLogger.init(); // 初始化日志对象
         // 1. 创建 P6eWebsocketClientApplication 对象
         P6eWebsocketClientApplication application = P6eWebsocketClientApplication.run(P6eNioModel.class);
-
-//        // 2. 连接 websocket 的地址
-//        application.connect(new P6eConfig("ws://111.229.238.242:7510/ws",
-//                new P6eActuatorDefault()));
 
         // 请求的请求头数据
         application.connect(new P6eConfig("wss://danmuproxy.douyu.com:8506/",
